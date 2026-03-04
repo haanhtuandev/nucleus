4b1cbb5563bf60f62506545623066af0a3e8f58b
haanhtuandev <tuanha41105@gmail.com>
2026-03-04 15:20:53 +0700

    fix godotenv for deploy

--- cmd/blog-service/main.go
+++ cmd/blog-service/main.go
@@ -13,10 +13,7 @@ import (
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+	_ = godotenv.Load()
 	dbURL := os.Getenv("DB_URL")
 	secret_key := os.Getenv("SECRET")
 	if secret_key == "" {
